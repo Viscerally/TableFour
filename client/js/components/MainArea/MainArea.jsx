00e8e6a2b0852132ef04a5ff59cd5082ba3a918f
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-25 00:45:04 -0700

    added an example of api

--- client/js/components/MainArea/MainArea.jsx
+++ client/js/components/MainArea/MainArea.jsx
@@ -1,12 +1,40 @@
 import React, { Component, Fragment } from 'react';
+import fetch from 'cross-fetch';
 import Navbar from '../Navbar.jsx';
 
 export default class MainArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: []
+    };
+    this.showLocation = this.showLocation.bind(this);
+  }
+
+  showLocation() {
+    console.log(this.state.locations);
+    return this.state.locations.map(
+      loc =>
+      <li key={loc.Id}>
+        <img src={loc.image} alt={loc.name} />
+        {loc.name}
+      </li>
+    )
+  }
+
+  componentDidMount() {
+    fetch('/api/locations')
+      .then(res => res.json())
+      .then(locations => this.setState({ locations }));
+  }
+
   render() {
     return (
       <Fragment>
         <Navbar />
-        <div>Hello MainArea!</div>
+        <div clasName='home-page-container'>
+          {this.showLocation()}
+        </div>
       </Fragment>
     );
   }
