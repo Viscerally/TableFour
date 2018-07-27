523c22cb4c3a858292dcf41f316e6fe2239353d8
byeong0430 <byeong.kim0430@gmail.com>
2018-07-27 18:24:54 +0000

    set up massive js in server.js

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const querystring = require('querystring');
-const request = require('request');
+// require massive js
+const massive = require('massive');
 
 // include the api router
 const apiRoutes = require('../routes/api');
@@ -14,16 +14,37 @@ const apiRoutes = require('../routes/api');
 // constants
 const PORT = process.env.PORT || 3001;
 const ENV = process.env.NODE_ENV || 'development';
-const redirect_uri = process.env.REDIRECT_URI || `http://localhost:${PORT}/callback`
-const client_id = process.env.SPOTIFY_CLIENT_ID;
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-// set up middleware
-// all static files are in /bundle
-app.use(express.static(__dirname + '/build'));
-// set up /api path for all api routes
-app.use('/api', apiRoutes);
+const connectionString = process.env.DATABASE_URL;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} in ${ENV} mode.`);
-});
+massive(connectionString)
+  .then(massiveInstance => {
+    console.log('Connection to PSQL established.')
+
+    // set up middleware
+    // all static files are in /bundle
+    app.use(express.static(__dirname + '/build'));
+    // set up /api path for all api routes
+    app.use('/api', apiRoutes);
+
+    app.set('db', massiveInstance);
+    const db = app.get('db');
+
+    // massive js example
+    var newUser = {
+      email: "test@test.com",
+      first: "Joe",
+      last: "Test"
+    };
+    db.users.save(newUser, function (err, result) {
+      console.log(result);
+    });
+
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT} in ${ENV} mode.`);
+    });
+  })
+  .catch(err => {
+    console.log(err.stack);
+  })
