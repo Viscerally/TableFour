91d3fb94d3c42eaf03b299da9cd76caed228b8f6
byeong0430 <byeong.kim0430@gmail.com>
2018-07-28 01:25:41 +0000

    added massive .save() to insert form data into customers table

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const apiRouter = express.Router();
 
-apiRouter.get('/reservations', (req, res) => {
-  res.send('Return all reservations');
-})
-apiRouter.post('/reservations', (req, res) => {
-  res.send('Create new reservation in DB.');
-})
-apiRouter.post('/reservations/:res_id', (req, res) => {
-  res.send('Cancel a reservation.');
-})
-apiRouter.patch('/reservations', (req, res) => {
-  res.send('Patch me up!');
-})
-apiRouter.get('/reservations/:res_id/orders', (req, res) => {
-  res.send('All orders associated with a reservation');
-})
-apiRouter.patch('/reservations/:res_id/orders/:order_id', (req, res) => {
-  res.send('Update the order');
-})
-apiRouter.get('/categories/:cat_id/menu_items', (req,res) => {
-  res.send('Return a list of all menu items associated with a category');
-})
+module.exports = function (db) {
+  apiRouter.get('/reservations', (req, res) => {
+    res.send('Return all reservations');
+  })
+  apiRouter.post('/reservations', (req, res) => {
+    // get the form data and capitalise the first letter of name
+    const formData = req.body;
+    formData.name = formData.name.replace(/^\w/, chr => chr.toUpperCase());
 
-module.exports = apiRouter;
+    // save the result into customers tb
+    db.customers.save(formData)
+      .then(result => {
+        console.log(result);
+        console.log('result');
+      })
+      .catch(err => {
+        res.status(500).send('Internal error');
+        console.log(err.stack);
+      });
+    res.send('Create new reservation in DB.');
+  })
+  apiRouter.post('/reservations/:res_id', (req, res) => {
+    res.send('Cancel a reservation.');
+  })
+  apiRouter.patch('/reservations', (req, res) => {
+    res.send('Patch me up!');
+  })
+  apiRouter.get('/reservations/:res_id/orders', (req, res) => {
+    res.send('All orders associated with a reservation');
+  })
+  apiRouter.patch('/reservations/:res_id/orders/:order_id', (req, res) => {
+    res.send('Update the order');
+  })
+  apiRouter.get('/categories/:cat_id/menu_items', (req, res) => {
+    res.send('Return a list of all menu items associated with a category');
+  })
+
+
+  // get the massiveJS instance saved in app object
+  // console.log(db);
+  return apiRouter;
+}
