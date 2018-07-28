b72b3d45a0414f5ce5773b11557829e262f2c65f
byeong0430 <byeong.kim0430@gmail.com>
2018-07-28 01:52:58 +0000

    massiveJS save customer and reservation data into db

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,21 +6,47 @@ module.exports = function (db) {
     res.send('Return all reservations');
   })
   apiRouter.post('/reservations', (req, res) => {
-    // get the form data and capitalise the first letter of name
+    // get the form data
     const formData = req.body;
-    formData.name = formData.name.replace(/^\w/, chr => chr.toUpperCase());
+    // deconstruct req.body object
+    const { name, phone, group_size, email } = req.body;
 
-    // save the result into customers tb
-    db.customers.save(formData)
-      .then(result => {
-        console.log(result);
-        console.log('result');
+    // compile all customer data
+    const customerData = {
+      // capitalise the first letter of name
+      name: name.replace(/^\w/, chr => chr.toUpperCase()),
+      phone,
+      email
+    };
+
+    // save customerData into customers tb
+    db.customers.save(customerData)
+      .then(customer => {
+
+        // create an object representing reservation data
+        const resoData = {
+          placement_time: new Date(),
+          status: 'waiting',
+          customer_id: customer.id,
+          group_size
+        };
+        // save reservation data
+        db.reservations.save(resoData)
+          .then(reservation => {
+            // return customer and reservation json
+            res.status(200).json({ customer, reservation });
+          })
+          .catch(err => {
+            res.status(500).send('Internal error');
+            console.log(err.stack);
+          })
+
       })
       .catch(err => {
         res.status(500).send('Internal error');
         console.log(err.stack);
       });
-    res.send('Create new reservation in DB.');
+
   })
   apiRouter.post('/reservations/:res_id', (req, res) => {
     res.send('Cancel a reservation.');
