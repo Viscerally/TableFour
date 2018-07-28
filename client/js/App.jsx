bae667301100b684457dc9eddb22433c18067fe9
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-28 11:53:41 -0700

    moved handleFormSubmission function to BookingForm component

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -1,48 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MainArea from './components/MainArea/MainArea.jsx';
 import HostArea from './components/HostArea/HostArea.jsx';
+
 import io from 'socket.io-client';
 import '../scss/application.scss';
-import { getAllReservations, makeReservation } from '../libs/reservation-func.js';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      socket: '',
-      reservations: []
-    };
-
-    this.handleResoFormSubmit = this.handleResoFormSubmit.bind(this);
-  }
-
-  // make a POST request with form data
-  handleResoFormSubmit(event) {
-    // prevent default GET request
-    event.preventDefault();
-    // take out obj keys from event.target
-    const { name, phone, group_size, email } = event.target;
-    // create JSON with name, phone, and email
-    const body = JSON.stringify({
-      name: name.value,
-      phone: phone.value,
-      group_size: group_size.value,
-      email: email.value
-    });
-    // make a POST request to /api/reservations
-    // NOTE: specify the content type to application/json
-
-    makeReservation(body)
-      .then(() => {
-        getAllReservations()
-          .then(reservations => {
-            // save all reso data to state
-            this.setState({ reservations });
-          })
-          .catch(err => { console.log(err) });
-      })
-      .catch(err => { console.log(err) });
+    this.state = { socket: '' };
   }
 
   componentDidMount() {
@@ -61,13 +28,7 @@ export default class App extends Component {
       <Router>
         <div>
           <Route path="/admin" component={HostArea} />
-          <Route path="/home"
-            render={props => (
-              <MainArea {...props}
-                handleResoFormSubmit={this.handleResoFormSubmit}
-              />
-            )}
-          />
+          <Route path="/home" component={MainArea} />
         </div>
       </Router>
     );
