8614e9737507ac44fa8a80c896b82929b9bb507c
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-28 11:57:05 -0700

    created successful booking alert

--- client/js/components/MainArea/BookingForm.jsx
+++ client/js/components/MainArea/BookingForm.jsx
@@ -27,13 +27,8 @@ export default class BookingForm extends Component {
     // NOTE: specify the content type to application/json
 
     makeReservation(body)
-      .then(() => {
-        getAllReservations()
-          .then(reservations => {
-            // save all reso data to state
-            this.setState({ reservations });
-          })
-          .catch(err => { console.log(err) });
+      .then(response => {
+        console.log(response);
       })
       .catch(err => { console.log(err) });
   }
