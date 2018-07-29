e40ce70c609ac2a4ade209dabb9b68aa82955b56
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-28 18:59:45 -0700

    removed constructor and componentDidMount

--- client/js/App.jsx
+++ client/js/App.jsx
@@ -3,26 +3,9 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import MainArea from './components/MainArea/MainArea.jsx';
 import HostArea from './components/HostArea/HostArea.jsx';
 
-import io from 'socket.io-client';
 import '../scss/application.scss';
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { socket: '' };
-  }
-
-  componentDidMount() {
-    // after all components are mounted, socket disappears
-    // this needs to be placed in state
-    const socket = io('http://localhost:3001');
-    this.setState({ socket });
-
-    socket.on('connect', () => {
-      console.log('sup guy');
-    });
-  }
-
   render() {
     return (
       <Router>
