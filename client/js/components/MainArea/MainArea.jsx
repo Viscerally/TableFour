ab4ef84099042c295ce02a9b233885a1426b81c2
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-28 19:07:50 -0700

    fix: duplicate reservation table.

--- client/js/components/MainArea/MainArea.jsx
+++ client/js/components/MainArea/MainArea.jsx
@@ -33,21 +33,12 @@ export default class MainArea extends Component {
               </article>
             </div>
           </div>
-          <div className='tile is-parent'>
-            <article className='tile is-child box'>
-              <div className='content'>
-                <h3 className='title is-3'>CURRENT RESERVATIONS</h3>
-                <ReservationDashboard />
-              </div>
-            </article>
-          </div>
-        </div>
           <div className='tile menu-tile is-4'>
             <Menu />
           </div>
         </main>
         <footer></footer>
-      </div>
+      </div >
     );
   }
 }
