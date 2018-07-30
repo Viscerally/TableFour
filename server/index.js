6feac34d11490013579fbf5f8189a812be70bb3d
jdpaterson <jaydpaterson@gmail.com>
2018-07-30 11:47:13 -0700

    Reconfigured Server and ports

--- server/index.js
+++ server/index.js
@@ -1,28 +1,5 @@
 require('dotenv').config();
-
-// constants
-//const connectionString = process.env.DATABASE_URL;
-// const bodyParser = require('body-parser');
-// const massive = require('massive');
-// app.use(express.static(__dirname + '/build'));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// const countClients = ws => Object.keys(ws.sockets.connected).length;
-// console.log(`${countClients(io)} CLIENT(S) CONNECTED`);
-// console.log(`${countClients(io)} CLIENT(S) CONNECTED`);
-
-// connect to PSQL using massive js
-/*
-massive(connectionString)
-  .then(massiveInstance => {
-    console.log('Connection to PSQL established.');
-    //const apiRoutes = require('../routes/api/index')(massiveInstance, io);
-    //app.use('/api', apiRoutes);
-  })
-  .catch(err => {
-    console.log(err.stack);
-  });
-*/
+const bodyParser = require('body-parser');
 
 //PORT for Express Server, Sockets will use the same server and port
 const PORT = process.env.PORT || 3001;
@@ -31,14 +8,32 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const massive = require('massive');
+const connectionString = process.env.DATABASE_URL;
+const countClients = Object.keys(io.sockets.connected).length;
+
+app.use(express.static(__dirname + '/build'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 io.on('connection', socket => {
-  socket.on('join', (data) => {    
+  console.log(`${countClients} CLIENT(S) CONNECTED`);
+  socket.on('join', (data) => {
     socket.emit('messages', 'Hello world from server');
   })
   socket.on('disconnect', () => {
-
+    console.log(`${countClients} CLIENT(S) CONNECTED`);
   })
 });
+
+massive(connectionString)
+  .then(massiveInstance => {
+    console.log('Connection to PSQL established.');
+    const apiRoutes = require('../routes/api/index')(massiveInstance, io);
+    app.use('/api', apiRoutes);
+  })
+  .catch(err => {
+    console.log(err.stack);
+  });
 
 server.listen(3001);
