ae3e418995822622862c5ab04f8ee1f00b917f1e
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-07-30 21:23:50 +0000

    Added and tested  POST route for posting  new order to database

--- client/js/components/MainArea/Menu.jsx
+++ client/js/components/MainArea/Menu.jsx
@@ -22,16 +22,19 @@ export default class Menu extends Component {
       })
   }
 
-  addToOrder(menuItem) {
-    fetch('/api/orders/order_id', {
+  addToOrder(ev, menuItem) {
+    ev.preventDefault()
+    fetch('/api/orders/1', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: menuItem
+      body: JSON.stringify(menuItem)
     })
-    .then(() => { resolve({ response: "Added succcesfully to the order" }); })
-    .catch(err => { reject(err.stack) });
-
-
+    .then((res) => { 
+      console.log(res)
+    })
+    .catch(err => { 
+      console.log(err) 
+    }); 
   } 
 
   render() {
@@ -55,7 +58,7 @@ export default class Menu extends Component {
 
             <div className="title">{menuItem.name}
               <div className="price">{menuItem.price}</div>
-              <button onClick={addToOrder} className="button is-danger">Add to your order</button>
+              <button onClick={(e) => this.addToOrder(e, menuItem)} className="button is-danger">Add to your order</button>
               </div>
           </div>
         )
