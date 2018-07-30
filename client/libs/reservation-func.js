24b7f10d1fd0ad0b3dc533edfcbdbb4b86812d8c
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-30 16:19:43 -0700

    added customised functions to a separate file

--- client/libs/reservation-func.js
+++ client/libs/reservation-func.js
@@ -1,21 +1,37 @@
-module.exports = {
-  getAllReservations() {
-    return new Promise((resolve, reject) => {
-      fetch('/api/reservations')
-        .then(response => response.json())
-        .then(reservations => { resolve(reservations) })
-        .catch(err => { reject(err.stack) });
+// check if res_code already exists
+const reservationExist = (reservations, res_code) => {
+  // check if the new res_code exists in the state reservation data
+  const booleanArray = reservations.map(reservation => reservation.res_code === res_code);
+  // check if there is any "true" in the array. if so, this means customer's trying to
+  // UPDATE data rather than adding new data
+  return booleanArray.some(el => el === true);
+};
+
+const returnResoArray = (reservations, newReservation) => {
+  const { res_code } = newReservation;
+
+  if (reservationExist(reservations, res_code)) {
+    // CASE 1: updating existing data
+    return reservations.map(reservation => {
+      if (reservation.res_code === res_code) {
+        return newReservation;
+      } else {
+        return reservation;
+      }
     });
-  },
-  makeReservation(data) {
-    return new Promise((resolve, reject) => {
-      fetch('/api/reservations', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: data
-      })
-        .then(() => { resolve({ response: "Booking successful" }); })
-        .catch(err => { reject(err.stack) });
-    });
+  } else {
+    // CASE 2: adding new reservation data
+    return [...reservations, newReservation];
   }
 };
+
+const getAllReservations = () => {
+  return new Promise((resolve, reject) => {
+    fetch('/api/reservations')
+      .then(response => response.json())
+      .then(reservations => { resolve(reservations) })
+      .catch(err => { reject(err.stack) });
+  });
+};
+
+module.exports = { getAllReservations, returnResoArray };
