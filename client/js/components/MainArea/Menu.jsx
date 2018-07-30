7d830377a289970adbda9c0fa6ceb4c1dbb9a7e0
jdpaterson <jaydpaterson@gmail.com>
2018-07-29 17:35:09 -0700

    Carousel is now populating dynamically

--- client/js/components/MainArea/Menu.jsx
+++ client/js/components/MainArea/Menu.jsx
@@ -5,36 +5,47 @@ export default class Menu extends Component {
   constructor(props){
     super();
 
+  }
+
+  componentDidMount(){
     fetch('/api/menu_items')
-      .then(function(response) { return response.json(); })
+      .then(response => {
+        return response.json();
+      })
       .then(data => {
-        console.log(data);
+        this.setState({
+          menu_items: data
+        })
       })
       .catch(err => {
         reject(err.stack);
       })
   }
 
-  componentDidMount(){
-    const carousels = bulmaCarousel.attach();
-    console.log('Carousels: ', carousels);
-  }
   render() {
+
+    let menuItems;
+
+    if (this.state){
+
+      menuItems = this.state.menu_items.map((menuItem) => {
+        return (
+          <div key={menuItem.id} className='carousel-item has-background'>
+            <img className="is-background" src={menuItem.img_url} alt="" width="300" height="300" />
+            <div className="title">{menuItem.name}</div>
+          </div>
+        )
+      })
+
+      const carousels = bulmaCarousel.attach();
+      console.log(menuItems);
+
+    }
+
     return (
         <div className='carousel carousel-animated carousel-animate-slide'>
           <div className='carousel-container'>
-            <div className='carousel-item has-background is-active'>
-              <img className="is-background" src="https://placebear.com/250/250" alt="" width="250" height="250" />
-              <div className="title">Beary Christmas</div>
-            </div>
-            <div className='carousel-item has-background is-active'>
-              <img className="is-background" src="https://placebear.com/250/250" alt="" width="250" height="250" />
-              <div className="title">Happy Pawnnakah</div>
-            </div>
-            <div className='carousel-item has-background is-active'>
-              <img className="is-background" src="https://placebear.com/250/250" alt="" width="250" height="250" />
-              <div className="title">Beary Christmas</div>
-            </div>
+              {menuItems}
           </div>
           <div className="carousel-navigation">
             <div className="carousel-nav-left">
