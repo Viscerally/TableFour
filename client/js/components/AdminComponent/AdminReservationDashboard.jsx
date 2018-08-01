1c2751ea4b74f21cadcb7e3c713ed1d716083674
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-01 14:57:12 -0700

    refactored code

--- client/js/components/AdminComponent/AdminReservationDashboard.jsx
+++ client/js/components/AdminComponent/AdminReservationDashboard.jsx
@@ -1,38 +1,32 @@
 import React, { Component, Fragment } from 'react';
-import io from 'socket.io-client';
 import StatusButton from './StatusButton.jsx';
-import { getAllReservations } from '../../../libs/reservation-func.js';
+
+// default table header
+const createDefaultHeader = () => {
+  return (
+    <tr>
+      <th>#</th>
+      <th>SIZE</th>
+      <th>NAME</th>
+      <th>ORDERED?</th>
+      <th>STATUS</th>
+    </tr>
+  );
+};
 
 export default class AdminReservationDashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      socket: '',
-      reservations: []
-    };
-  }
-
   selectBtn = (event, status) => {
     // get value of 'data-key' which is === primary key of reservation
     const id = event.target.getAttribute('data-key');
     // send the object to web socket
-    this.state.socket.emit('updateReservationStatus', { id, status });
+    this.props.socket.emit('updateReservationStatus', { id, status });
   }
 
-  makeTable = () => {
+  makeTable = reservations => {
     let sizeSum = 0;
-    const tHeader = (
-      <tr>
-        <th>#</th>
-        <th>SIZE</th>
-        <th>NAME</th>
-        <th>ORDERED?</th>
-        <th>STATUS</th>
-      </tr>
-    );
 
     // loop through table rows
-    const cells = this.state.reservations.map((reservation, index) => {
+    const cells = reservations.map((reservation, index) => {
       // add the group size
       sizeSum += reservation.group_size;
       const { id, group_size, name, order_id, status } = reservation;
@@ -60,68 +54,25 @@ export default class AdminReservationDashboard extends Component {
       )
     });
 
-    let stats = (
-      <tr>
-        <th colSpan='5'>
-          Total of {this.state.reservations.length} groups ({sizeSum} people) waiting..
-        </th>
-      </tr>
-    );
+    let stats = `Total of ${reservations.length} groups (${sizeSum} people) waiting..`;
 
     return (
       <Fragment>
-        <thead>{stats}{tHeader}</thead>
+        <thead>
+          <tr>
+            <th colSpan='5'>{stats}</th>
+          </tr>
+          {createDefaultHeader()}
+        </thead>
         <tbody>{cells}</tbody>
       </Fragment>
     );
   };
 
-  componentDidMount() {
-    // WEBSOCKET
-    // initiate socket and save it in the state
-    const socket = io('http://localhost:3001');
-    this.setState({ socket });
-
-    // INITIAL RESERVATION DATA
-    // get all reservations
-    getAllReservations()
-      .then(reservations => {
-        // save all reso data to state
-        this.setState({ reservations });
-      })
-      .catch(err => { console.log(err) });
-
-    // SOCKET CONNECTION
-    // as customer submits the form, the form data's broadcast back here
-    // add the new reservation data into the existing state
-    socket.on('connect', () => {
-      console.log('Connected to websocket');
-      socket.on('news', () => {
-        getAllReservations()
-          .then(reservations => {
-            // save all reso data to state
-            this.setState({ reservations });
-          })
-          .catch(err => { console.log(err) });
-      });
-
-      socket.on('newStatus', newStatus => {
-        const { id, status } = newStatus;
-        const reservations = this.state.reservations.map(reservation => {
-          if (reservation.id == id) {
-            reservation.status = status;
-          }
-          return reservation;
-        });
-        this.setState({ reservations });
-      })
-    });
-  }
-
   render() {
     return (
       <table className='table is-striped is-hoverable is-fullwidth reservation-dashboard'>
-        {this.makeTable()}
+        {this.makeTable(this.props.reservations)}
       </table>
     );
   }
