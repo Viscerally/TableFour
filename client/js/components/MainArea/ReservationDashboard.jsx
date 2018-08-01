37d541aa7efb5faf8683b5e0e4d1435884bd0f63
Byeong Kim <byeong.kim0430@gmail.com>
2018-07-31 22:02:12 -0700

    did some major refactoring

--- client/js/components/MainArea/ReservationDashboard.jsx
+++ client/js/components/MainArea/ReservationDashboard.jsx
@@ -1,36 +1,31 @@
 import React, { Component, Fragment } from 'react';
-import { getAllReservations, returnResoArray } from '../../../libs/reservation-func.js';
+
+// default table header
+const createDefaultHeader = () => {
+  return (
+    <tr>
+      <th>#</th>
+      <th>NAME</th>
+      <th>SIZE</th>
+      <th></th>
+    </tr>
+  );
+};
 
 export default class ReservationDashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reservations: [],
-      res_code: ''
-    };
-  }
-
-  makeTable = () => {
+  makeTable = (reservations, res_code) => {
     // set sizeSum to 0 before calculating how many people are ahead of the current customer
     let sizeSum = 0;
     let stats = '';
-
-    // default table header
-    const tHeader = (
-      <tr>
-        <th>#</th>
-        <th>NAME</th>
-        <th>SIZE</th>
-        <th></th>
-      </tr>
-    );
     let options = '';
 
     // loop through table rows
-    const cells = this.state.reservations.map((reservation, index) => {
+    const cells = reservations.map((reservation, index) => {
       // add the group size
       sizeSum += reservation.group_size;
-      if (this.state.res_code == reservation.res_code) {
+
+      if (res_code == reservation.res_code) {
+        // create stats for the current reservation
         stats = `Your position: #${index + 1} (${sizeSum - reservation.group_size} people ahead)`;
 
         // option for the selected reservation
@@ -43,7 +38,6 @@ export default class ReservationDashboard extends Component {
           </a>
         );
       } else {
-        // hide customer name other than the current customer's name
         options = '';
       }
 
@@ -51,8 +45,8 @@ export default class ReservationDashboard extends Component {
       // show the first 3 rows and then skip to the current user
       const visibleRowCut = 3;
       let { name, group_size } = reservation;
-      const klassName = (this.state.res_code == reservation.res_code) ? 'is-selected' : '';
-      name = (this.state.res_code == reservation.res_code) ? name : '...';
+      const klassName = (res_code == reservation.res_code) ? 'is-selected' : '';
+      name = (res_code == reservation.res_code) ? name : '...';
 
       if (index < visibleRowCut) {
         // first 3 rows
@@ -64,7 +58,7 @@ export default class ReservationDashboard extends Component {
             <td>{options}</td>
           </tr>
         );
-      } else if (this.state.res_code == reservation.res_code) {
+      } else if (res_code == reservation.res_code) {
         // current customer's reservation row
         return (
           <tr key={reservation.id} className={klassName}>
@@ -83,71 +77,28 @@ export default class ReservationDashboard extends Component {
       }
     });
 
-    if (this.state.res_code === null) {
-      stats = `Total of ${this.state.reservations.length} groups (${sizeSum} people) waiting..`;
+    if (res_code === null) {
+      stats = `Total of ${reservations.length} groups (${sizeSum} people) waiting..`;
     }
+
     return (
       <Fragment>
         <thead>
           <tr>
             <th colSpan='4'>{stats}</th>
           </tr>
-          {tHeader}
+          {createDefaultHeader()}
         </thead>
         <tbody>{cells}</tbody>
       </Fragment>
     );
   };
 
-  componentDidMount() {
-    // RESERVATION ID
-    // check if res_code's passed as a URL param
-    // if it exists, save it in the state. if not, save it as null
-    this.setState(oldState => {
-      let { res_code } = this.props.urlParams;
-      res_code = (this.props.urlParams.res_code) || null;
-
-      oldState.res_code = res_code;
-      // pass res_code to the parent component
-      this.props.getResCode(res_code);
-      return oldState;
-    });
-
-    // INITIAL RESERVATION DATA
-    // get all reservations
-    getAllReservations()
-      .then(reservations => {
-        // save all reso data to state
-        this.setState({ reservations });
-      })
-      .catch(err => { console.log(err) });
-
-    // SOCKET CONNECTION
-    // as customer submits the form, the form data's broadcast back here
-    // add the new reservation data into the existing state
-    const { socket } = this.props;
-    socket.on('connect', () => {
-      console.log('Connected to websocket');
-      socket.on('news', newRecord => {
-        const newReservation = { ...newRecord.customer, ...newRecord.reservation }
-
-        this.setState(oldState => {
-          const { res_code } = newReservation;
-
-          // pass res_code to the parent component
-          this.props.getResCode(res_code);
-
-          const reservations = returnResoArray(oldState.reservations, newReservation);
-          return { ...oldState, res_code, reservations };
-        });
-      });
-    });
-  }
-
   render() {
+    const { reservations, formData: { res_code } } = this.props;
     return (
       <table className='table is-striped is-hoverable is-fullwidth reservation-dashboard'>
-        {this.makeTable()}
+        {this.makeTable(reservations, res_code)}
       </table>
     );
   }
