e1a85fea5d53d95efcfa3d93430e8fa054275727
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-04 16:32:51 -0700

    refactored category component

--- client/js/components/Category.jsx
+++ client/js/components/Category.jsx
@@ -2,23 +2,15 @@ import React, { Component } from 'react';
 
 
 export default class Category extends Component {
-  render () {
-      return (
-        <a key={this.props.menu.id} onClick={() => {this.props.setMenu(this.props.menu)}}>
-        <article className="menuCategories">
-            <div className="tile is-ancestor">
-              <div className="tile is-parent">
-                <article className="tile is-child box">
-                  <div className="tile">
-                    <p className="title">{this.props.menu.name}</p>
-                  </div>        
-                </article>
-              </div>
-            </div>
-          </article>
-          
-        </a>
-        
-      )
+  render() {
+    const { menu, setMenu } = this.props;
+    return (
+      <a key={menu.id} onClick={() => { setMenu(menu) }}>
+          <span className="title is-4">
+            {menu.name}
+          </span>
+      </a>
+
+    )
   }
 }
