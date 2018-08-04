927d2196364f309fe8ff06393a161a3756534369
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-04 14:36:07 -0700

    simplified websocket logic

--- libs/socket-helpers.js
+++ libs/socket-helpers.js
@@ -2,22 +2,22 @@
 const countClients = ws => Object.keys(ws.sockets.connected).length;
 
 // SET UP BROADCAST LOGIC
-const broadcastData = (socket, emitTo, dataToSend, admins, clients) => {
+const broadcastResos = (io, socket, emitTo, dataToSend, admins, sendToAll) => {
   // https://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender
-  // original sender & admins get full data back
-  Object.keys(admins).forEach(adminId => {
-    socket.broadcast.to(adminId).emit(emitTo, dataToSend);
-  });
-  socket.emit(emitTo, dataToSend);
+  if (sendToAll) {
+    io.emit(emitTo, dataToSend);
+  } else {
+    // FIRST, SEND DATA BACK TO THE ORIGINAL SENDER
+    socket.emit(emitTo, dataToSend);
 
-  // everybody else get everything EXCEPT name & res_code
-  dataToSend.customer.name = '...';
-  dataToSend.reservation.isViewable = false;
-  Object.keys(clients).forEach(clientId => {
-    if (clientId !== socket.id) {
-      socket.broadcast.to(clientId).emit(emitTo, dataToSend);
-    }
-  });
+    // NEXT, SEND THE SAME DATASET TO ALL ADMINS BUT AFTER CHANGING RES_CODE TO ''
+    // OTHERWISE SUBMIT BUTTON DISAPPEARS AND UPDATE AND CANCEL BUTTONS SHOW UP (NOT GOOD)
+    dataToSend.reservation.res_code = '';
+    // original sender & admins get full data back
+    Object.keys(admins).forEach(adminId => {
+      socket.broadcast.to(adminId).emit(emitTo, dataToSend);
+    });
+  }
 };
 
-module.exports = { countClients, broadcastData };
+module.exports = { countClients, broadcastResos };
