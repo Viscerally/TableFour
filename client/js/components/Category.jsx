c0ebaf02b558b41346adf8c27fcc47170c91dae9
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-04 23:58:47 -0700

    fix: wrapped each category with article

--- client/js/components/Category.jsx
+++ client/js/components/Category.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
-
 export default class Category extends Component {
   render() {
     const { menu, setMenu } = this.props;
     return (
-      <a key={menu.id} onClick={() => { setMenu(menu) }}>
+      <a onClick={() => setMenu(menu)} className="tile is-parent">
+        <article className="tile is-child box menuCategories has-text-centered">
           <span className="title is-4">
             {menu.name}
           </span>
+        </article>
       </a>
-
     )
   }
 }
