4630b0ba2888f5b9a87019ed08e5ebd3e4b955ca
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-06 21:08:40 -0700

    for development io.connect websocket location should be specified

--- client/js/components/ComponentHandler.jsx
+++ client/js/components/ComponentHandler.jsx
@@ -5,7 +5,8 @@ import MainComponent from './MainComponent.jsx';
 export default function (props) {
   const isAdmin = props.match.url === '/admin' ? true : false;
   const res_code = props.match.params.res_code === undefined ? null : props.match.params.res_code;
-  const socket = io.connect();
+  const socket = (process.env.NODE_ENV === 'development') ?
+    io.connect('http://localhost:3001') : io.connect();
 
   // NEED TO GENERATE THE RES_CODE AS SOON AS USER LOGS IN
   // STORE IT IN STATE
