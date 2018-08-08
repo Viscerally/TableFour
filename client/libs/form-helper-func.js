95cf00b47047bf5f5bd7a864807ebcf049919810
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-08 12:37:16 -0700

    added blank error object

--- client/libs/form-helper-func.js
+++ client/libs/form-helper-func.js
@@ -2,6 +2,13 @@ const namifyStr = str => str.toLowerCase().trim().replace(/^\w/, letter => lette
 
 const getOnlyNumbers = str => str.replace(/\D/g, '');
 
+const blankError = () => {
+  return {
+    code: '',
+    message: ''
+  };
+};
+
 const blankReservation = () => {
   return {
     placement_time: '',
@@ -10,26 +17,26 @@ const blankReservation = () => {
     group_size: '',
     customer_id: '',
     res_code: ''
-  }
-}
+  };
+};
 
 const blankCustomer = () => {
   return {
     name: '',
     phone: '',
     email: ''
-  }
-}
+  };
+};
 
 const resoData = state => {
-  const { customer, reservation, path } = state;
+  const { customer, reservation } = state;
   return {
     name: namifyStr(customer.name),
     phone: getOnlyNumbers(customer.phone),
     group_size: reservation.group_size,
     email: customer.email,
     res_code: reservation.res_code
-  }
-}
+  };
+};
 
-module.exports = { blankReservation, blankCustomer, resoData };
+module.exports = { blankError, blankReservation, blankCustomer, resoData };
