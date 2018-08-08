9a5ebcedae1b4847f76cf04297c452b07aa37985
Byeong Kim <byeong.kim0430@gmail.com>
2018-08-07 19:05:51 -0700

    removed unnecessary line breaker

--- client/js/components/Category.jsx
+++ client/js/components/Category.jsx
@@ -5,7 +5,6 @@ export default class Category extends Component {
     const { menu, setMenu } = this.props;
     return (
       <a onClick={() => setMenu(menu)} className="tile is-parent">
-      
         <article className="tile is-child box has-text-centered">
           <span className="category-name title is-5">
             {menu.name}
